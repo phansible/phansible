79483a85a4a725151bef7db6ec2bb4cfd65b6b2b
Fabio Ribeiro <fabiorphp@gmail.com>
2014-07-05 00:45:49 -0300

    Default Controller
    * Improve tests for Default Controller
    * Change docs path url for use the config

---
+++ tests/src/Phansible/Controller/DefaultControllerTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Phansible\Controller;
+
+class DefaultControllerTests extends \PHPUnit_Framework_TestCase
+{
+    private $controller;
+    private $twig;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->controller = new DefaultController();
+        $this->twig       = $this->getMockBuilder('\Twig_Environment')
+            ->setMethods(['render'])
+            ->getMock();
+    }
+
+    public function tearDown()
+    {
+        $this->controller = null;
+        $this->twig = null;
+    }
+
+    public function testShouldRenderIndexAction()
+    {
+        $container = new \Pimple();
+
+        $this->twig->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->equalTo('index.html.twig'),
+                $this->arrayHasKey('config')
+            );
+
+        $container['twig'] = $this->twig;
+        $container['config'] = [];
+
+        $this->controller->setPimple($container);
+        $this->controller->indexAction();
+
+    }
+
+    public function testShouldRenderUsageActionWhenDocFileNotExists()
+    {
+        $container = new \Pimple();
+
+        $this->twig->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->equalTo('docs.html.twig'),
+                $this->equalTo(['content' => ''])
+            );
+
+        $container['twig'] = $this->twig;
+        $container['docs.path'] = '';
+        $doc = '';
+
+        $this->controller->setPimple($container);
+        $this->controller->usageAction($doc);
+    }
+
+    public function testShouldRenderUsageActionWhenFileExists()
+    {
+        $container = new \Pimple();
+
+        $this->twig->expects($this->once())
+            ->method('render')
+            ->with(
+                $this->equalTo('docs.html.twig'),
+                $this->callback(function($param){
+                    return array_key_exists('content', $param) && strpos($param['content'], 'ansible');
+                })
+            );
+
+        $docFile = new \SplFileObject('/tmp/phansible.md', 'w+');
+        $docFile->fwrite('Phansible');
+
+        $doc = 'phansible';
+
+        $container['twig'] = $this->twig;
+        $container['docs.path'] = '/tmp';
+
+        $this->controller->setPimple($container);
+        $this->controller->usageAction($doc);
+
+        unlink($docFile->getPathname());
+    }
+}
