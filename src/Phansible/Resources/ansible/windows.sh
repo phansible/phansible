96645a0bbe4b02e222ceb8f884ae97c1535f732d
fideloper <fideloper@gmail.com>
2014-10-28 08:52:02 -0400

    typos in comments

--- src/Phansible/Resources/ansible/windows.sh
+++ src/Phansible/Resources/ansible/windows.sh
@@ -8,8 +8,8 @@ sudo apt-get update
 
 # Decide on package to install for `add-apt-repository` command
 #
-# USE_COMMON=1 when using a distributino over 12.04
-# USE_COMMON== when using a distribution older or at 12.04
+# USE_COMMON=1 when using a distribution over 12.04
+# USE_COMMON=0 when using a distribution at 12.04 or older
 USE_COMMON=$(echo "$DISTRIB_RELEASE > 12.04" | bc)
 
 if [ "$USE_COMMON" -eq "1" ];
