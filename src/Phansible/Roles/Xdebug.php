4f5ac24e03218632c10d1a99ca4f43423d8dea4f
Mark Pfrengle <mark.pfrengle@gmail.com>
2015-08-30 09:54:36 +0200

    * apply xdebug settings if given * added xdebug setting preset if enable

--- src/Phansible/Roles/Xdebug.php
+++ src/Phansible/Roles/Xdebug.php
@@ -3,9 +3,11 @@
 namespace Phansible\Roles;
 
 use Phansible\Role;
+use Phansible\RoleValuesTransformer;
+use Phansible\Model\VagrantBundle;
 use Phansible\RoleWithDependencies;
 
-class Xdebug implements Role, RoleWithDependencies
+class Xdebug implements Role, RoleWithDependencies, RoleValuesTransformer
 {
     public function getName()
     {
@@ -33,5 +35,18 @@ class Xdebug implements Role, RoleWithDependencies
           'install' => 0,
           'settings' => [],
         ];
+    }
+
+    public function transformValues(array $values, VagrantBundle $vagrantBundle)
+    {
+        if ($values['install'] == 1) {
+            $values['settings'] = [
+                'xdebug.remote_enable' => 1,
+                'xdebug.remote_connect_back' => 1,
+                'xdebug.max_nesting_level' => 300
+            ];
+        }
+
+        return $values;
     }
 }
