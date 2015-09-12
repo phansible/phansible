3a343046366e22c61b6e05dcfa26d85b6432921a
Mark Pfrengle <mark.pfrengle@gmail.com>
2015-09-12 09:11:12 +0200

    * moved storage of xdebug settings to ansible defaults instead of using corresponding php role class

--- src/Phansible/Roles/Xdebug.php
+++ src/Phansible/Roles/Xdebug.php
@@ -3,11 +3,9 @@
 namespace Phansible\Roles;
 
 use Phansible\Role;
-use Phansible\RoleValuesTransformer;
-use Phansible\Model\VagrantBundle;
 use Phansible\RoleWithDependencies;
 
-class Xdebug implements Role, RoleWithDependencies, RoleValuesTransformer
+class Xdebug implements Role, RoleWithDependencies
 {
     public function getName()
     {
@@ -32,21 +30,7 @@ class Xdebug implements Role, RoleWithDependencies, RoleValuesTransformer
     public function getInitialValues()
     {
         return [
-          'install' => 0,
-          'settings' => [],
+          'install' => 0
         ];
-    }
-
-    public function transformValues(array $values, VagrantBundle $vagrantBundle)
-    {
-        if ($values['install'] == 1) {
-            $values['settings'] = [
-                'xdebug.remote_enable' => 1,
-                'xdebug.remote_connect_back' => 1,
-                'xdebug.max_nesting_level' => 300
-            ];
-        }
-
-        return $values;
     }
 }
