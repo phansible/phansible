f91e69c72a38784b4b4133bca096c1a5b0d31168
Marco De Bortoli <me@debo.io>
2020-03-07 15:10:54 +0000

    Remove old docroot

--- web.disabled/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = new \Phansible\Application(__DIR__ . '/..');
-$app->configure(__DIR__ . '/../app/config/config.config');
-$app->run();
--- web.disabled/index_dev.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = new \Phansible\Application(__DIR__ . '/..', true);
-$app->configure(__DIR__ . '/../app/config/config.config');
-$app->run();
